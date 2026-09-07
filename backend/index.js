1d0ea436cfa39ea840968589107fc64720655cfc
leonor <thyselfseeking@gmail.com>
2026-09-08 04:02:49 +0530

    Configure backend for Render deployment

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cartRoutes = require("./routes/cartRoutes");
-const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -29,22 +28,12 @@ app.use('/api/analytics', require('./routes/analyticsRoutes'));
 app.use('/api/categories', require('./routes/categoryRoutes'));
 app.use('/api/cart', cartRoutes);
 
-//server frontend in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.get('/{*splat}', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
-    });
-} else {
-        app.get("/", (req,res) => {
-        res.send("ShopNest Backend is working properly!");
-    });
-}
+app.get("/", (req, res) => {
+    res.send("ShopNest Backend is working properly!");
+});
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-
-
