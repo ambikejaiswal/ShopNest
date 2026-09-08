a0837ec5786a6d212dba445a0b70e435a6738f50
leonor <thyselfseeking@gmail.com>
2026-09-08 05:30:37 +0530

    Allow Vercel frontend in CORS

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'https://shop-nest-jet.vercel.app'
     ],
     credentials: true
 }));
